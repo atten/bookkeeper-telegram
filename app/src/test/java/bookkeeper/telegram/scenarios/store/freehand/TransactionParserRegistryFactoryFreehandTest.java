0591c0638eeca0119be51d8cd3493063cd6da4a2
atten <art@force.fm>
2023-08-24 20:28:00 +0300

    refactor tests

--- app/src/test/java/bookkeeper/telegram/scenarios/store/freehand/TransactionParserRegistryFactoryFreehandTest.java
+++ app/src/test/java/bookkeeper/telegram/scenarios/store/freehand/TransactionParserRegistryFactoryFreehandTest.java
@@ -1,29 +1,16 @@
 package bookkeeper.telegram.scenarios.store.freehand;
 
-import bookkeeper.entities.TelegramUser;
 import bookkeeper.services.matchers.ExpenditureMatcherByMerchant;
-import bookkeeper.services.registries.TransactionParserRegistry;
 import bookkeeper.services.repositories.AccountRepository;
 import bookkeeper.services.repositories.AccountTransactionRepository;
 import bookkeeper.services.repositories.MerchantExpenditureRepository;
 import bookkeeper.services.repositories.TelegramUserRepository;
+import bookkeeper.telegram.scenarios.store.TransactionParserRegistryCommonTest;
 import com.pengrad.telegrambot.model.User;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.text.ParseException;
-import java.util.Objects;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class TransactionParserRegistryFactoryFreehandTest {
-    private static TransactionParserRegistry registry;
-    private static TelegramUser user;
+class TransactionParserRegistryFactoryFreehandTest extends TransactionParserRegistryCommonTest {
 
     @BeforeAll
     static void prepare() {
@@ -37,29 +24,7 @@ class TransactionParserRegistryFactoryFreehandTest {
 
         user = userRepository.getOrCreate(telegramUser);
         registry = new TransactionParserRegistryFactoryFreehand(accountRepository, transactionRepository, expenditureMatcherByMerchant).create();
-    }
-
-    /**
-     * Just check valid messages are parsed but transactions list is empty
-     */
-    @Test
-    void parseEmpty() throws ParseException, URISyntaxException, IOException {
-        var path = Path.of(Objects.requireNonNull(this.getClass().getResource("/emptyMessagesFreehand.txt")).toURI());
-        var lines = Files.readAllLines(path).toArray(new String[0]);
-        var transactions = registry.parseMultiple(lines, user);
-        assert transactions.isEmpty();
-    }
-
-    /**
-     * Just check invalid message results in ParseError
-     */
-    @Test
-    void parseFail() throws URISyntaxException, IOException {
-        var path = Path.of(Objects.requireNonNull(this.getClass().getResource("/errorMessagesAll.txt")).toURI());
-        var lines = Files.readAllLines(path);
-
-        for (var line : lines) {
-            assertThrows(ParseException.class, () -> registry.parse(line, user));
-        }
+        validMessagesPath = "/validMessagesFreehand.txt";
+        emptyMessagesPath = "/emptyMessagesFreehand.txt";
     }
 }
--- app/src/test/java/bookkeeper/telegram/scenarios/store/tinkoff/TransactionParserRegistryFactoryTinkoffTest.java
+++ app/src/test/java/bookkeeper/telegram/scenarios/store/tinkoff/TransactionParserRegistryFactoryTinkoffTest.java
@@ -1,29 +1,16 @@
 package bookkeeper.telegram.scenarios.store.tinkoff;
 
-import bookkeeper.entities.AccountTransaction;
-import bookkeeper.entities.TelegramUser;
 import bookkeeper.services.repositories.AccountRepository;
 import bookkeeper.services.repositories.MerchantExpenditureRepository;
 import bookkeeper.services.repositories.TelegramUserRepository;
 import bookkeeper.services.matchers.ExpenditureMatcherByMerchant;
-import bookkeeper.services.registries.TransactionParserRegistry;
+import bookkeeper.telegram.scenarios.store.TransactionParserRegistryCommonTest;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import com.pengrad.telegrambot.model.User;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.text.ParseException;
-import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class TransactionParserRegistryFactoryTinkoffTest {
-    private static TransactionParserRegistry registry;
-    private static TelegramUser user;
+class TransactionParserRegistryFactoryTinkoffTest extends TransactionParserRegistryCommonTest {
 
     @BeforeAll
     static void prepare() {
@@ -36,41 +23,7 @@ class TransactionParserRegistryFactoryTinkoffTest {
 
         user = userRepository.getOrCreate(telegramUser);
         registry = new TransactionParserRegistryFactoryTinkoff(accountRepository, expenditureMatcherByMerchant).create();
-    }
-
-    /**
-     * Just check valid messages are parsed into transactions
-     */
-    @Test
-    void parseOk() throws ParseException, URISyntaxException, IOException {
-        var path = Path.of(Objects.requireNonNull(this.getClass().getResource("/validMessagesTinkoff.txt")).toURI());
-        var lines = Files.readAllLines(path).toArray(new String[0]);
-        var transactions = registry.parseMultiple(lines, user);
-        assert transactions.size() == lines.length;
-        assert transactions.stream().noneMatch(AccountTransaction::isEmpty);
-    }
-
-    /**
-     * Just check valid messages are parsed but transactions list is empty
-     */
-    @Test
-    void parseEmpty() throws ParseException, URISyntaxException, IOException {
-        var path = Path.of(Objects.requireNonNull(this.getClass().getResource("/emptyMessagesTinkoff.txt")).toURI());
-        var lines = Files.readAllLines(path).toArray(new String[0]);
-        var transactions = registry.parseMultiple(lines, user);
-        assert transactions.isEmpty();
-    }
-
-    /**
-     * Just check invalid message results in ParseError
-     */
-    @Test
-    void parseFail() throws URISyntaxException, IOException {
-        var path = Path.of(Objects.requireNonNull(this.getClass().getResource("/errorMessagesAll.txt")).toURI());
-        var lines = Files.readAllLines(path);
-
-        for (var line : lines) {
-            assertThrows(ParseException.class, () -> registry.parse(line, user));
-        }
+        validMessagesPath = "/validMessagesTinkoff.txt";
+        emptyMessagesPath = "/emptyMessagesTinkoff.txt";
     }
 }
